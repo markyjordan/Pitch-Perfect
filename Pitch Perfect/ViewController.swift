34e05decab6c93b023d5b5e036ce43da2f40669e
Mark Jordan <markybjordan@gmail.com>
2020-03-11 04:05:19 -0700

    Relinked  the 'recordAudio' IB Action and added an 'isEnabled' to the 'viewDidLoad' function.
    The 'isEnabled' property of the stopRecording button was set to false (disabled)inside the 'vieDidLoad' function.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-    
     @IBOutlet weak var recordButton: UIButton!
-    
     @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
