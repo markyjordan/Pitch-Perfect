cc7c745c906d9f44fcf75a8347e99fc3444ed32a
Mark Jordan <markybjordan@gmail.com>
2020-03-11 02:51:46 -0700

    Added an IB Outlet for the 'Recocd' button and the 'Stop Recording' button.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
